cda3551077dfa481c2fb6a36dfb90455383e5e02
AnnaKohler <anna.kohler.610@gmail.com>
2017-01-09 15:30:15 +0400

    January Update

--- app/src/main/java/ru/startandroid/criminalintent/CrimeFragment.java
+++ app/src/main/java/ru/startandroid/criminalintent/CrimeFragment.java
@@ -1,13 +1,22 @@
 package ru.startandroid.criminalintent;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+
+import java.util.Date;
+import java.util.UUID;
 
 /**
  * Created by Anna on 18.12.2016.
@@ -16,16 +25,51 @@ import android.widget.EditText;
 public class CrimeFragment extends Fragment {
     private Crime mCrime;
     private EditText mTitleField;
+    private Button mDateButton;
+    private CheckBox mSolvedCheckBox;
+    private static final String DIALOG_DATE = "date";
+    private static final int REQUEST_DATE=0;
+
+    public void updateDate() {
+        mDateButton.setText(mCrime.getDate().toString());
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mCrime = new Crime();
+        UUID crimeId = (UUID)getArguments().getSerializable("EXTRA_CRIME_ID");
+        mCrime = CrimeLab.get(getActivity()).getCrime(crimeId);
     }
+
+
+    public static CrimeFragment newInstance(UUID crimeId){
+        Bundle args = new Bundle();
+        args.putSerializable("EXTRA_CRIME_ID", crimeId);
+        CrimeFragment fragment = new CrimeFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != Activity.RESULT_OK) return;
+        if (requestCode == REQUEST_DATE) {
+            Date date = (Date)data
+                    .getSerializableExtra("EXTRA_DATE");
+            mCrime.setDate(date);
+            updateDate();
+        }
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_crime, parent, false);
-        mTitleField=(EditText) v.findViewById(R.id.crime_title);
+        //заполняется представление фрагемента
+
+        mTitleField=(EditText) v.findViewById(R.id.crime_title); //ссылка на поле ввода
+        mTitleField.setText(mCrime.getTitle());
+
         mTitleField.addTextChangedListener(new TextWatcher() {
             public void onTextChanged(
                     CharSequence c, int start, int before, int count) {
@@ -33,12 +77,36 @@ public class CrimeFragment extends Fragment {
             }
             public void beforeTextChanged(
                     CharSequence c, int start, int count, int after) {
-// Здесь намеренно оставлено пустое место
+            // Здесь намеренно оставлено пустое место
             }
             public void afterTextChanged(Editable c) {
-// И здесь тоже
+            // И здесь тоже
             }
         });
+
+        mDateButton =(Button) v.findViewById(R.id.crime_date);
+        updateDate();
+
+        mDateButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                FragmentManager fm = getActivity()
+                        .getSupportFragmentManager();
+                DatePickerFragment dialog = DatePickerFragment.newInstance(mCrime.getDate());
+                dialog.setTargetFragment(CrimeFragment.this, REQUEST_DATE);
+                dialog.show(fm, DIALOG_DATE);
+            }
+        });
+
+        mSolvedCheckBox=(CheckBox)v.findViewById(R.id.crime_solved);
+        mSolvedCheckBox.setChecked(mCrime.isSolved());
+
+        mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                mCrime.setSolved(isChecked);
+            }
+        });
+
         return v;
     }
 }
